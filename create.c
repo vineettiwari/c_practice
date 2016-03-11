bf019105086f9cc8114a4f6065ab27dd57fe30fa
vineetTiwari <vineet01@hotmail.com>
2016-03-11 00:32:28 -0800

    Refactured the code for create.c

--- create.c
+++ create.c
@@ -28,44 +28,52 @@ void trim_comma(char *str_value)
 
 bool parse_line(char *line, struct account *customer_account)
 {
-	bool didRead = false;
-	int i = 0, success = 0;
+	//bool didRead = false;
+	//int i = 0, success = 0;
+	
+	size_t success = 0;
 	char 	temp_accnum[BUFF_SIZE], temp_fname[BUFF_SIZE],
 		temp_lname[BUFF_SIZE], temp_accbal[BUFF_SIZE],
 		temp_lpay[BUFF_SIZE];
 	
+	/*
 	while (i < sizeof(line)) {
 		success = sscanf(line, "%s %s %s %s %s", temp_accnum, 
 			temp_fname, temp_lname, temp_accbal, temp_lpay);
 		i++;
 	}
+	*/
+	
+	success = sscanf(line, "%s %s %s %s %s", temp_accnum, 
+			temp_fname, temp_lname, temp_accbal, temp_lpay);
 	
 	trim_comma(temp_accnum);
 	trim_comma(temp_fname);
 	trim_comma(temp_lname);
 	trim_comma(temp_accbal);
 	trim_comma(temp_lpay);
-	
-	//strcpy(customer_account->first_name, temp_fname);
-	//strcpy(customer_account->last_name, temp_lname);
+
 	success += sscanf(temp_fname, "%s", customer_account->first_name);
 	success += sscanf(temp_lname, "%s", customer_account->last_name);
 	success += sscanf(temp_accnum, "%d", &(customer_account->account_number));
 	success += sscanf(temp_accbal, "%lf", &(customer_account->account_balance));
 	success += sscanf(temp_lpay, "%lf", &(customer_account->last_payment_amount));
 	
-	if (success == 10)
+	/*if (success == 10)
 		didRead = true;
+	*/
 		
-	return didRead;
+	return (success == 10);
 }
 
 int main()
 {
-	FILE *ftext_ptr, *fbinary_ptr;
+	FILE *ftext_ptr = NULL;
+	FILE *fbinary_ptr = NULL;
 	char line[LINE_SIZE];
-	size_t counter = 0;
 	struct account customer_account;
+	
+	//size_t counter = 0;
 	
 	/* read in text mode */
 	ftext_ptr = fopen("account.csv", "r");
@@ -85,10 +93,16 @@ int main()
 	
 	putchar('\n');
 	while (fgets(line, sizeof(line), ftext_ptr)) {
-		if (parse_line(line, &customer_account) == 1) {
-			fwrite(&customer_account,
-				sizeof(struct account), 1, fbinary_ptr);
-			counter++;
+		if (parse_line(line, &customer_account)) {
+			if (fwrite(&customer_account,
+				sizeof(struct account), 1, fbinary_ptr) != 1) {
+				fprintf(stderr,
+					"\nError - Unable to save to disk\n");
+				exit(EXIT_FAILURE);
+			} else {
+				fprintf(stderr,
+					"\nWarning: Misformed line\n");
+			}
 		}
 	}
 
@@ -108,6 +122,17 @@ int main()
 	}		
 	
 	putchar('\n');
+	while (fread(&customer_account,
+			sizeof(struct account), 1, fbinary_ptr) == 1) {
+		printf("%d %s %s %.2lf %.2lf\n",
+				customer_account.account_number,
+				customer_account.first_name,
+				customer_account.last_name,
+				customer_account.account_balance,
+				customer_account.last_payment_amount);
+	}
+	
+	/*
 	for ( int i = 0; i < counter; i++) {
 		fread(&customer_account,
 			sizeof(struct account), 1, fbinary_ptr);
@@ -119,6 +144,7 @@ int main()
 				customer_account.account_balance,
 				customer_account.last_payment_amount);
 	}
+	*/
 	
 	if (fclose(fbinary_ptr) != 0)
 		fprintf(stderr, "\nError: Unable to "
