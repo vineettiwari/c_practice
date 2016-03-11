436769bdcf2f2457c7d7a52982e92df2435af4ed
vineetTiwari <vineet01@hotmail.com>
2016-03-10 18:48:00 -0800

    Implemented parse() function in create.c.

--- create.c
+++ create.c
@@ -6,9 +6,10 @@
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 
-#define BUFF_SIZE 16
+#define BUFF_SIZE 20
 #define LINE_SIZE 256
 
 struct account {
@@ -17,10 +18,58 @@ struct account {
 	float	account_balance, last_payment_amount;
 };
 
-bool parse_row(char *row, struct account *customer_account)
+float converter(char *str_value)
+{
+	int  i, len, result = 0;
+	len = strlen(str_value);
+
+	for (i = 0; i < len; i++) {
+		result = result * 10 + ( str_value[i] - '0' );
+	}
+	
+	return result;
+}
+
+void trim_comma(char *str_value)
+{
+	int length = strlen(str_value);
+	
+	if (str_value[length - 1] == ',')
+		str_value[length - 1] = '\0';
+} 
+
+bool parse_line(char *line, struct account *customer_account)
 {
 	bool didRead = false;
+	int i = 0, success = 0;
+	char 	temp_accnum[BUFF_SIZE], temp_fname[BUFF_SIZE],
+		temp_lname[BUFF_SIZE], temp_accbal[BUFF_SIZE],
+		temp_lpay[BUFF_SIZE];
 	
+	while (i < sizeof(line)) {
+		success = sscanf(line, "%s %s %s %s %s", temp_accnum, 
+			temp_fname, temp_lname, temp_accbal, temp_lpay);
+		i++;
+	}
+	
+	trim_comma(temp_accnum);
+	trim_comma(temp_fname);
+	trim_comma(temp_lname);
+	trim_comma(temp_accbal);
+	trim_comma(temp_lpay);
+	
+	customer_account->account_number = (int)converter(temp_accnum);
+	customer_account->account_balance = converter(temp_accbal);
+	customer_account->last_payment_amount = converter(temp_lpay);
+	
+	printf("%d %s %s %f %f\n",
+	customer_account->account_number, temp_fname, temp_lname,
+	customer_account->account_balance,
+	customer_account->last_payment_amount);
+	
+	if (success == 5)
+		didRead = true;
+		
 	return didRead;
 }
 
@@ -28,37 +77,35 @@ int main()
 {
 	FILE *ftext_ptr, *fbinary_ptr;
 	char line[LINE_SIZE];
+	struct account customer_account;
 	
-	ftext_ptr = fopen("account.csv", "r");		/* read text mode */
+	/* read in text mode */
+	ftext_ptr = fopen("account.csv", "r");
 	if (!ftext_ptr) {
 		fprintf(stderr,
 			"\nError: Unable to open file - account.csv\n");
 		exit(EXIT_FAILURE);
 	}
-
-	fbinary_ptr = fopen("account.dat", "wb");	/* write and read */
-	if (!fbinary_ptr) {				/* binary mode	  */
+	
+	/* write in binary mode */
+	fbinary_ptr = fopen("account.dat", "wb");
+	if (!fbinary_ptr) {
 		fprintf(stderr, "\nError: Unable to "
 			"create/open file - account.dat\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while () {  /* TODO */
-
+	while (fgets(line, sizeof(line), ftext_ptr)) {  /* TODO */
+		parse_line(line, &customer_account);
 	}
 
-
-	if (fclose(ftext_ptr) != 0) {
+	if (fclose(ftext_ptr) != 0)
 		fprintf(stderr, "\nError: Unable to "
 			"close file - account.csv\n");
-		exit(EXIT_FAILURE);
-	}
 	
-	if (fclose(fbinary_ptr) != 0) {
+	if (fclose(fbinary_ptr) != 0)
 		fprintf(stderr, "\nError: Unable to "
 			"close file - account.dat\n");
-		exit(EXIT_FAILURE);
-	}
 	
 	printf("\nFile copied successfully!\n\n");
 	return EXIT_SUCCESS;
